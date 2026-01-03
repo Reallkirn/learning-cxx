c12e24fde7cef3446ce652c26db6cb58310fab0a
Reallkirn <503403969@qq.com>
2026-01-03 11:38:02 +0800

    练习完成

---
+++ exercises/20_function_template/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "../exercise.h"
+
+// READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
+// TODO: 将这个函数模板化
+// int plus(int a, int b) {
+//     return a + b;
+// }
+
+template<typename T>
+T plus(T a, T b) {
+    return a + b;
+}
+
+template<typename T>
+bool approximately_equal(T a, T b, T epsilon = std::numeric_limits<T>::epsilon()) {
+    // 如果两个数非常接近
+    if (a == b) return true;
+    
+    // 计算相对误差
+    T diff = std::fabs(a - b);
+    T max_val = std::max(std::fabs(a), std::fabs(b));
+    
+    return diff <= epsilon * max_val;
+}
+
+int main(int argc, char **argv) {
+    ASSERT(plus(1, 2) == 3, "Plus two int");
+    ASSERT(plus(1u, 2u) == 3u, "Plus two unsigned int");
+
+    // THINK: 浮点数何时可以判断 ==？何时必须判断差值？
+    ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
+    ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
+    // TODO: 修改判断条件使测试通过
+    ASSERT(approximately_equal(plus(0.1, 0.2),0.3), "How to make this pass?");
+
+    return 0;
+}
---
+++ exercises/33_std_accumulate/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "../exercise.h"
+#include <numeric>
+
+// READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
+
+int main(int argc, char **argv) {
+    using DataType = float;
+    int shape[]{1, 3, 224, 224};
+    // TODO: 调用 `std::accumulate` 计算：
+    //       - 数据类型为 float；
+    //       - 形状为 shape；
+    //       - 连续存储；
+    //       的张量占用的字节数
+    // int size =
+    int total_elements = std::accumulate(
+        std::begin(shape),    // 数组开始
+        std::end(shape),      // 数组结束
+        1,                    // 乘法初始值为1
+        std::multiplies<int>() // 乘法操作
+    );
+    
+    // 2. 计算总字节数
+    int size = total_elements * sizeof(DataType);
+    ASSERT(size == 602112, "4x1x3x224x224 = 602112");
+    return 0;
+}
